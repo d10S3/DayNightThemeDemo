0d2a669dbf80604d78068cc982fd28fa2863d9b0
추상범 <sbchoo@sk.com>
2020-07-21 17:53:02 +0900

    package name change

--- app/src/main/java/com/d10s3/daynightthemedemo/ui/preferences/PreferencesFragment.kt
+++ app/src/main/java/com/d10s3/daynightthemedemo/ui/preferences/PreferencesFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.d10s3.daynightthemedemo.R
+import com.d10s3.daynightthemedemo.ThemeManager
+import kotlinx.android.synthetic.main.fragment_preferences.*
+import kotlinx.android.synthetic.main.fragment_preferences.view.*
 
 class PreferencesFragment : Fragment() {
 
@@ -22,10 +27,27 @@ class PreferencesFragment : Fragment() {
         preferencesViewModel =
                 ViewModelProviders.of(this).get(PreferencesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_preferences, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        preferencesViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        when(ThemeManager.getTheme()) {
+            AppCompatDelegate.MODE_NIGHT_NO ->
+                root.radioLightButton.isChecked = true
+            AppCompatDelegate.MODE_NIGHT_YES ->
+                root.radioDarkButton.isChecked = true
+            else ->
+                root.radioDefaultButton.isChecked = true
+        }
+
+        root.radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId) {
+                radioLightButton.id ->
+                    ThemeManager.applyTheme(ThemeManager.ThemeMode.LIGHT)
+                radioDarkButton.id ->
+                    ThemeManager.applyTheme(ThemeManager.ThemeMode.DARK)
+                else ->
+                    ThemeManager.applyTheme(ThemeManager.ThemeMode.DEFAULT)
+
+            }
+        }
         return root
     }
 }
